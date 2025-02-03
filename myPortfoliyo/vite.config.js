2d58b6d7c02d2be6b962f232fbfab31628089d5d
kirtan1313 <mkirtan013@gmail.com>
2025-02-03 17:31:48 +0530

    up1

--- myPortfoliyo/vite.config.js
+++ myPortfoliyo/vite.config.js
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory for the build
-    rollupOptions: {
-      input: 'index.html', // Define the entry point for your application
-    },
+    outDir: 'build', // Specify the output directory for the build
+    
   },
 })
